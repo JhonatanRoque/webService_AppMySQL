f79195d892c80a1143cf2703dc47ff0153d94b8a
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-10-07 19:41:56 -0600

    Eliminar categoria

--- categoria.php
+++ categoria.php
@@ -20,9 +20,28 @@ class Categoria {
             return -1;
         }                        
     }
+
+    public static function eliminar_Categoria($idCat) {
+        include("connection_db.php");
+        $query = "DELETE FROM tb_categoria WHERE id_cat = ?";
+        try{    
+          $link=conexion();    
+          $comando = $link->prepare($query);
+          $comando->execute(array($idCat));
+          $count = $comando->rowCount();
+         //echo $count;
+          if($count > 0){
+              return 1;
+          }else{
+              return 0;
+          }
+        } catch (PDOException $e) {
+            return -1;
+        }                        
+    }
     
     //by Tec. Francisco Abarca 
     //Modificado por: Tec. Francisco Abarca
-    //Fecha modificación: 06/10/2022 03:40 pm
+    //Fecha modificación: 07/10/2022 07:40 pm
 }
 ?>
---
+++ eliminarCategoria.php
@@ -0,0 +1,26 @@
+<?php
+
+include('categoria.php'); 
+
+//$idCategoria = $_POST['id_categoria'];
+$idCategoria = 57;
+
+if (($idCategoria!="") {
+
+    $resultado = Mantenimiento::eliminar_Categoria($idCategoria);
+
+    if($resultado == 1) {
+        header('Content-type: application/json; charset=utf-8');
+        $json_string = json_encode(array("estado" => 1,"mensaje" => "Registro eliminado correctamente."));
+        echo $json_string;
+    }  else {
+        header('Content-type: application/json; charset=utf-8');
+        $json_string = json_encode(array("estado" => 2,"mensaje" => "Error al eliminar la categoria $idCategoria."));
+		echo $json_string;
+    }
+}
+
+//by Tec. Francisco Abarca 
+//Modificado por: Tec. Francisco Abarca
+//Fecha modificación: 07/10/2022 07:40 pm
+?>
